fb3d4140a87a44e4fcfa48d9f5f13304f6bcd05e
Brandon <brandon.cebi.pro@gmail.com>
2023-03-08 11:48:59 +0100

    create basic form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import ReactPDF, { PDFViewer } from "@react-pdf/renderer";
 import { PDFGenerator } from "./components/PDFGenerator";
+import FormDevis from "./components/FormDevis";
 
 function App() {
   return (
     <div className="test">
-      <PDFViewer height={"100%"} width={"100%"}>
+      {/* <PDFViewer height={"100%"} width={"100%"}>
         <PDFGenerator />
-      </PDFViewer>
+      </PDFViewer> */}
+      <FormDevis />
     </div>
   );
 }
--- client/src/components/FormDevis.tsx
+++ client/src/components/FormDevis.tsx
@@ -1,1 +1,57 @@
-export default function formDevis() {}
+export default function htmlFormDevis() {
+  return (
+    <form className="w-full max-w-lg m-auto mt-9">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3 mb-6 md:mb-0">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
+          >
+            Name
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3 mb-6 md:mb-0">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="phone"
+          >
+            Phone number
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="phone"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3 mb-6 md:mb-0">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="email"
+            type="text"
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Génerer le PDF
+      </button>
+    </form>
+  );
+}
